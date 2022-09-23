e7eff463062cda41e7f80fbca326adc307ed2e02
Enoch 叶 <enochye@Enochidr-MBA.local>
2022-09-23 22:33:34 +0800

    Refactoring UI

--- Flip_Chess/Board.py
+++ Flip_Chess/Board.py
@@ -1,45 +1,58 @@
 # coding: utf-8
 import time
 
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+
 
 class Board:
     def __init__(self):
         self._board = []
         # 棋盘初始化
         for i in range(8):
-            tmp = ['·' for i in range(8)]
+            tmp = [EMPTY for i in range(8)]
             self._board.append(tmp)
-        self._board[3][3], self._board[4][4] = 'X', 'X'
-        self._board[3][4], self._board[4][3] = 'O', 'O'
+        self._board[3][3], self._board[4][4] = BLACK, BLACK
+        self._board[3][4], self._board[4][3] = WHITE, WHITE
         # 判断是否可以产生翻转的八个方向
-        self._directions = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
-        
+        self._directions = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1],
+        ]
+
     def show_board(self):
         # 在UI中打印棋盘信息
         pass
 
     def display_board(self):
         # 在控制台上打印棋盘
-        print('    1    2    3    4    5    6    7    8')
+        print("    1    2    3    4    5    6    7    8")
         for index, board in enumerate(self._board):
             print(index + 1, board)
 
     def put_piece(self, x, y, chess_piece):
-        '''
+        """
 
         :param x: 坐标x, 对应行
         :param y: 坐标y, 对应列
         :param chess_piece: 玩家棋子
         :return:
-        '''
-        assert isinstance(chess_piece, str)
+        """
+        assert chess_piece in [WHITE, BLACK]
         # 不可重复落子
-        if self._board[x][y] != '·':
-            print('Cannot repeat the drop...')
+        if self._board[x][y] != EMPTY:
+            print("Cannot repeat the drop...")
             return -1
 
         if self.drop(x, y, chess_piece):
-            if self.judge_end() == '·':
+            if self.judge_end() == EMPTY:
                 return 0
         else:
             return -1
@@ -54,13 +67,13 @@ class Board:
             return False
 
     def _judge_flip(self, x, y, chess_piece):
-        '''
+        """
         判断本次落子是否可以产生翻转
         :param x:
         :param y:
         :param chess_piece:
         :return:
-        '''
+        """
         results = []
         for direction in self._directions:
             i, j = x, y
@@ -72,7 +85,7 @@ class Board:
                 if i < 0 or j < 0 or i > 7 or j > 7:
                     can_flip = False
                     break
-                if self._board[i][j] == '·':
+                if self._board[i][j] == EMPTY:
                     can_flip = False
                     break
                 if self._board[i][j] == chess_piece:
@@ -100,7 +113,7 @@ class Board:
         strategy = []
         for i in range(8):
             for j in range(8):
-                if self._board[i][j] == '·':
+                if self._board[i][j] == EMPTY:
                     can_flip, _ = self._judge_flip(i, j, chess_piece)
                     if can_flip:
                         strategy.append([i, j])
@@ -108,51 +121,70 @@ class Board:
         return strategy
 
     # 此函数暂时无调用, 闲置
-    '''
+    """
     def judge_area(self, chess_piece):
         flip = False
         for i in range(8):
             for j in range(8):
-                if self._board[i][j] == '·':
+                if self._board[i][j] == EMPTY:
                     can_flip, _ = self._judge_flip(i, j, chess_piece)
                     flip |= can_flip
 
         return flip
-    '''
+    """
 
     def judge_end(self):
         # 判断全局是否仍有可以落子的坐标
         flip = False
         for i in range(8):
             for j in range(8):
-                if self._board[i][j] == '·':
-                    can_flip, _ = self._judge_flip(i, j, 'O')
+                if self._board[i][j] == EMPTY:
+                    can_flip, _ = self._judge_flip(i, j, WHITE)
                     flip |= can_flip
-                    can_flip, _ = self._judge_flip(i, j, 'X')
+                    can_flip, _ = self._judge_flip(i, j, BLACK)
                     flip |= can_flip
 
         if not flip:
             winner, _ = self.calculate_winner()
             return winner
 
-        return '·'
+        return EMPTY
 
     def calculate_winner(self):
         count_O = 0
         count_X = 0
         for i in range(8):
             for j in range(8):
-                if self._board[i][j] == 'O':
+                if self._board[i][j] == WHITE:
                     count_O += 1
-                if self._board[i][j] == 'X':
+                if self._board[i][j] == BLACK:
                     count_X += 1
         if count_O != count_X:
             if count_O > count_X:
-                return 'O', abs(count_O - count_X)
+                return WHITE, abs(count_O - count_X)
             else:
-                return 'X', abs(count_O - count_X)
+                return BLACK, abs(count_O - count_X)
         else:
-            return 'D', 0
+            return "D", 0
+
+    def get_xy_on_logic_state(self, x, y):
+        return self._board[x][y]
+
+    def get_logic_board(self):
+        pieces_state = [EMPTY for _ in range(64)]
+        for i in range(8):
+            for j in range(8):
+                pieces_state[i * 8 + j] = self._board[i][j]
+        return pieces_state
+
+    def reset(self):
+        self._board = []
+        # 棋盘初始化
+        for i in range(8):
+            tmp = [EMPTY for i in range(8)]
+            self._board.append(tmp)
+        self._board[3][3], self._board[4][4] = BLACK, BLACK
+        self._board[3][4], self._board[4][3] = WHITE, WHITE
 
 
 class Game:
@@ -171,13 +203,15 @@ class Game:
         status = 0
         count_mistake = 0
         current_player = self._black_player
-        while self._board.judge_end() == '·' and count_mistake < 3:
-            '''-------------------------------------------------------'''
+        while self._board.judge_end() == EMPTY and count_mistake < 3:
+            """-------------------------------------------------------"""
             # 以下输出模块需要在UI上相应体现
-            print(f'current player is: {current_player.name}')
-            '''-------------------------------------------------------'''
+            print(f"current player is: {current_player.name}")
+            """-------------------------------------------------------"""
             strategy = current_player(self._board)
-            status = self._board.put_piece(strategy[0], strategy[1], current_player.chess_piece)
+            status = self._board.put_piece(
+                strategy[0], strategy[1], current_player.chess_piece
+            )
             if status == 0:
                 count_mistake = 0
                 self._board.display_board()
@@ -186,18 +220,17 @@ class Game:
                 count_mistake += 1
 
         winner, d_value = self._board.calculate_winner()
-        '''-------------------------------------------------------'''
+        """-------------------------------------------------------"""
         # 以下输出模块需要在UI上相应体现
-        if winner == 'D':
-            print('Draw...')
-        elif winner == 'X':
-            print(f'The winner is {self._black_player.name} and win {d_value} pieces')
-        elif winner == 'O':
-            print(f'The winner is {self._white_player.name} and win {d_value} pieces')
-        '''-------------------------------------------------------'''
+        if winner == "D":
+            print("Draw...")
+        elif winner == BLACK:
+            print(f"The winner is {self._black_player.name} and win {d_value} pieces")
+        elif winner == WHITE:
+            print(f"The winner is {self._white_player.name} and win {d_value} pieces")
+        """-------------------------------------------------------"""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # debug here
     pass
-
---
+++ ui_main.py
@@ -0,0 +1,175 @@
+# encoding: utf-8
+from Flip_Chess.Board import Board
+
+WIDTH = 540
+HEIGHT = 540
+MARGIN = 30
+GRID = (WIDTH - 2 * MARGIN) / (8 - 1)
+PIECE = 60
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+
+
+import sys
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QIcon, QPalette, QPainter
+
+
+class LaBel(QLabel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setMouseTracking(True)
+
+    def enterEvent(self, e):
+        e.ignore()
+
+
+class GoBang(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.chessboard = Board()  # 棋盘类
+
+        palette1 = QPalette()  # 设置棋盘背景
+        palette1.setBrush(
+            self.backgroundRole(),
+            QtGui.QBrush(QtGui.QPixmap("img/chessboard.jpg").scaledToWidth(540)),
+        )
+        self.setPalette(palette1)
+        self.setCursor(Qt.PointingHandCursor)  # 鼠标变成手指形状
+
+        self.resize(WIDTH, HEIGHT)  # 固定大小 540*540
+        self.setMinimumSize(QtCore.QSize(WIDTH, HEIGHT))
+        self.setMaximumSize(QtCore.QSize(WIDTH, HEIGHT))
+
+        self.setWindowTitle("FlipChess")  # 窗口名称
+        self.setWindowIcon(QIcon("img/black.png"))  # 窗口图标
+
+        self.black = QPixmap("img/black.png")
+        self.white = QPixmap("img/white.png")
+
+        self.piece_now = BLACK  # 黑棋先行
+        self.my_turn = True  # 玩家先行
+        self.step = 0  # 步数
+        self.x, self.y = 1000, 1000
+
+        self.mouse_point = LaBel(self)  # 将鼠标图片改为棋子
+        self.mouse_point.setScaledContents(True)
+        self.mouse_point.setPixmap(self.black)  # 加载黑棋
+        self.mouse_point.setGeometry(270, 270, PIECE, PIECE)
+        self.pieces = [LaBel(self) for i in range(64)]  # 新建棋子标签，准备在棋盘上绘制棋子
+        for piece in self.pieces:
+            piece.setVisible(True)  # 图片可视
+            piece.setScaledContents(True)  # 图片大小根据标签大小可变
+
+        self.pieces_state = [EMPTY for _ in range(64)]
+        self.pieces_state = self.chessboard.get_logic_board()
+
+        self.mouse_point.raise_()  # 鼠标始终在最上层
+        self.ai_down = (
+            True  # AI已下棋，主要是为了加锁，当值是False的时候说明AI正在思考，这时候玩家鼠标点击失效，要忽略掉 mousePressEvent
+        )
+
+        self.setMouseTracking(True)
+        self.show()
+
+    def paintEvent(self, event):  # 画出指示箭头
+        qp = QPainter()
+        qp.begin(self)
+        self.drawLines(qp)
+        qp.end()
+
+    def mouseMoveEvent(self, e):  # 黑色棋子随鼠标移动
+        self.mouse_point.move(e.x() - 32, e.y() - 32)
+
+    def mousePressEvent(self, e):  # 玩家下棋
+        if e.button() == Qt.LeftButton and self.ai_down == True:
+            x, y = e.x(), e.y()
+            i, j = self.coordinate_transform_pixel2map(x, y)
+            if not i is None and not j is None:
+                if self.chessboard.get_xy_on_logic_state(i, j) == EMPTY:
+                    # 棋子落在空白处才进行反应，传入到chessboard进行处理，然后刷新GUI棋盘
+                    self.chessboard.put_piece(i, j, self.piece_now)
+                    self.update_UI_chessboard()
+                    # 落子完毕后，当前棋子取反
+                    self.piece_now = BLACK if self.piece_now == WHITE else BLACK
+
+    def update_UI_chessboard(self):
+        logic_chessboard = self.chessboard.get_logic_board()
+        for i in len(logic_chessboard):
+            for j in len(logic_chessboard[0]):
+                piece_index = 8 * i + j
+                if (
+                    self.pieces_state[piece_index]
+                    != self.chessboard.get_xy_on_logic_state[piece_index]
+                ):
+                    # 如果当前棋子非空，将棋子clear
+                    if self.pieces_state[piece_index] != EMPTY:
+                        self.pieces[piece_index].clear()
+                    if logic_chessboard[piece_index] == BLACK:
+                        self.pieces[piece_index].setPixmap(self.black)
+                    else:
+                        self.pieces[piece_index].setPixmap(self.white)
+                    x, y = self.coordinate_transform_map2pixel(i, j)
+                    self.pieces[piece_index].setGeometry(int(x), int(y), PIECE, PIECE)
+
+    def drawLines(self, qp):  # 指示AI当前下的棋子
+        if self.step != 0:
+            pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+            qp.setPen(pen)
+            qp.drawLine(self.x - 5, self.y - 5, self.x + 3, self.y + 3)
+            qp.drawLine(self.x + 3, self.y, self.x + 3, self.y + 3)
+            qp.drawLine(self.x, self.y + 3, self.x + 3, self.y + 3)
+
+    def coordinate_transform_map2pixel(self, i, j):
+        # 从 chessMap 里的逻辑坐标到 UI 上的绘制坐标的转换
+        return MARGIN + j * GRID - PIECE / 2, MARGIN + i * GRID - PIECE / 2
+
+    def coordinate_transform_pixel2map(self, x, y):
+        # 从 UI 上的绘制坐标到 chessMap 里的逻辑坐标的转换
+        i, j = int(round((y - MARGIN) / GRID)), int(round((x - MARGIN) / GRID))
+        # 有MAGIN, 排除边缘位置导致 i,j 越界
+        if i < 0 or i >= 8 or j < 0 or j >= 8:
+            return None, None
+        else:
+            return i, j
+
+    def gameover(self, winner):
+        if winner == BLACK:
+            reply = QMessageBox.question(
+                self,
+                "You Win!",
+                "Continue?",
+                QMessageBox.Yes | QMessageBox.No,
+                QMessageBox.No,
+            )
+        else:
+            reply = QMessageBox.question(
+                self,
+                "You Lost!",
+                "Continue?",
+                QMessageBox.Yes | QMessageBox.No,
+                QMessageBox.No,
+            )
+
+        if reply == QMessageBox.Yes:  # 复位
+            self.piece_now = BLACK
+            self.mouse_point.setPixmap(self.black)
+            for piece in self.pieces:
+                piece.clear()
+            self.chessboard.reset()
+            self.update()
+        else:
+            self.close()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = GoBang()
+    sys.exit(app.exec_())
